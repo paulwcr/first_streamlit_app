76ab4a21ead01bb0bf8cbdaf7157c29cfd2901a4
Paul <105895675+paulwcr@users.noreply.github.com>
2022-05-22 16:49:57 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,9 +30,9 @@ streamlit.dataframe(fruits_to_show)
 def get_fruityvice_data(this_fruit_choice):
  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) #take the json version of the response and normalize it
- return fruityvice_normailzed
-# New section to display the Fruity Vice API resonse
+ return fruityvice_normalized
 
+# New section to display the Fruity Vice API resonse
 streamlit.header('Fruityvice Fruit Advice!')
 try:
  fruit_choice = streamlit.text_input('What fruit would you like information about?')
@@ -48,13 +48,7 @@ except URLError as e:
   
 streamlit.write('The User entered', fruit_choice)
 
-#import requests
-
-
-
-
-
-
+#import request
 
 streamlit.stop()
 #import snowflake.connector
